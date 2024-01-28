b1ea14a772dd4ac96e9ba11d4039bcf350be550d
Yarheeguy <125282780+Yarheeguy@users.noreply.github.com>
2024-01-28 16:03:11 +0000

    1111

---
+++ ProjectMakeMeLaugh/Assets/Menu/Firedscriptover.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Firedscriptover : MonoBehaviour
+{
+    public void gameovergoback()
+    {
+
+    }
+
+    public void gameoverexit()
+    {
+        Application.Quit();
+        print("Game Exited");
+    }
+}
